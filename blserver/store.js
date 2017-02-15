0ba0cdaf48469761a1f4374689fc9cc51ae8978d
Roman Eremin) <roman.eremin@devexpress.com>
2017-02-15 12:18:19 +0300

    Add redux provider, hook redux devtools

--- blserver/store.js
+++ blserver/store.js
@@ -1,16 +1,16 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 
 export const reducer = (state = { lastUpdate: 0, light: false }, action) => {
   switch (action.type) {
-    case 'TICK': return { lastUpdate: action.ts, light: !!action.light }
+    //case 'TICK': return { lastUpdate: action.ts, light: !!action.light }
     default: return state
   }
 }
 
-export const startClock = () => dispatch => {
-  return setInterval(() => dispatch({ type: 'TICK', light: true, ts: Date.now() }), 500)
-}
+//export const startClock = () => dispatch => {
+//  return setInterval(() => dispatch({ type: 'TICK', light: true, ts: Date.now() }), 500)
+//}
 
 let store = null
 
@@ -19,7 +19,8 @@ export const initStore = (reducer, initialState, isServer) => {
     return createStore(reducer, initialState, applyMiddleware(thunkMiddleware))
   } else {
     if (!store) {
-      store = createStore(reducer, initialState, applyMiddleware(thunkMiddleware))
+      const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+      store = createStore(reducer, initialState, composeEnhancers(applyMiddleware(thunkMiddleware)))
     }
     return store
   }
