20f95e04345cb006c19a5fd1f80f025f9ac163ff
Roman Eremin) <roman.eremin@devexpress.com>
2017-02-21 12:27:10 +0300

    extract bootstrap component

--- blserver/containers/App.js
+++ blserver/containers/App.js
@@ -3,19 +3,25 @@ import React, {PropTypes} from 'react'
 import Head from 'next/head'
 import { NavBar } from '../components/NavBar'
 
+const BootstrapSetup = () => (
+<div>
+    <Head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" 
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <link rel="stylesheet" 
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" 
+        />
+    </Head>
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>     
+</div>
+)
+
 const App = ({nav, children}) => (
-    <div className="container">       
-        <Head>
-            <meta charset="utf-8"/>
-            <meta name="viewport" 
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-            <link rel="stylesheet" 
-            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" 
-            />
-        </Head>
-        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" ></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+    <div className="container">   
+       <BootstrapSetup/>     
        <NavBar path={nav.pathname}/>
         {children}
     </div>
